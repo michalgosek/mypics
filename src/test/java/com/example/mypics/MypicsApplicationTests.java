c08a370fa2b26f4f1be829c20821b08c2b0e53bf
Michal Gosek <inbox@michalgosek.com>
2021-04-11 08:46:54 +0200

    added spring boot with dependencies.

---
+++ src/test/java/com/example/mypics/MypicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
